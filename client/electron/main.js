a4bf7d3676894cace7145a8d727aa32588beb2e5
Brian Aung <brianaung16@gmail.com>
2024-01-23 18:07:45 +1100

    feat: create electron app

---
+++ client/electron/main.js
@@ -0,0 +1,40 @@
+// Note: I use nix so I installed electron as nixpkg rather than using npm,
+// because some required libraries were missing on my system if I use npm to install electron.
+import { app, BrowserWindow, screen as electronScreen } from "electron";
+
+const createMainWindow = () => {
+  let mainWindow = new BrowserWindow({
+    width: electronScreen.getPrimaryDisplay().workArea.width,
+    height: electronScreen.getPrimaryDisplay().workArea.height,
+    show: false,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+  const startURL = "http://localhost:3000";
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once("ready-to-show", () => mainWindow.show());
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on("activate", () => {
+    if (!BrowserWindow.getAllWindows().length) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
--- flake.nix
+++ flake.nix
@@ -5,18 +5,19 @@
 	};
 
   outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-    in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        name = "Standup-board build environment";
-        buildInputs = with pkgs; [
-          nodejs_18
-					prettierd
-        ];
-        shellHook = ''
-          echo "Welcome in $name"
-        '';
-      };
-    };
+	let
+	  pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+	in {
+	  devShells.x86_64-linux.default = pkgs.mkShell {
+		name = "Standup-board build environment";
+		buildInputs = with pkgs; [
+			nodejs_18
+			electron_28
+			prettierd
+		];
+		shellHook = ''
+		  echo "Welcome in $name"
+		'';
+	  };
+	};
 }
